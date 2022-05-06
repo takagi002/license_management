e6f16f12b550be21af53acc8177a252a1a197900
Rudolf Baun <rubait01@hs-esslingen.de>
2022-05-06 14:55:47 +0200

    Dao getList and entery Funktions added

--- src/main/java/de/hse/swb/jpa/orm/dao/UserDao.java
+++ src/main/java/de/hse/swb/jpa/orm/dao/UserDao.java
@@ -1,5 +1,25 @@
 package de.hse.swb.jpa.orm.dao;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import de.hse.swb.jpa.orm.model.User;
+
 public class UserDao {
+	@Inject
+	EntityManager em;
+	
+	public List<User> getUsers(){
+		TypedQuery<User> query = em.createQuery("SELECT u FROM User u", User.class);
+		List<User> results = query.getResultList();
+		return results;
+	}
+	
+	public User getUser(long id) {
+		return em.find(User.class, id);
+	}
 
 }

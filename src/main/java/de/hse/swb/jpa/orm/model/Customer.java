c5a00a1c3056c2b1ae0514e53f34bc7059dad488
robertfeo <rofeit00@hs-esslingen.de>
2022-05-06 12:15:28 +0200

    Customer Entity created

--- src/main/java/de/hse/swb/jpa/orm/model/Customer.java
+++ src/main/java/de/hse/swb/jpa/orm/model/Customer.java
@@ -1,5 +1,73 @@
 package de.hse.swb.jpa.orm.model;
 
-public class Customer {
+import java.util.List;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Customer")
+public class Customer {
+	@Id
+    @SequenceGenerator(name = "custSeq", sequenceName = "ZSEQ_CUS_ID", allocationSize = 1, initialValue = 10)
+    @GeneratedValue(generator = "custSeq")
+	@Column(name = "customerId")
+    private Long customerId;
+	
+	@Column(name = "name", length=60, nullable = false)
+    private String name;
+	
+	@Column(name = "department", length=60, nullable = false)
+    private String department;
+	
+	@Column(name = "adresse", length=100, nullable = false)
+    private String adresse;
+	
+	@OneToMany(mappedBy = "customer")
+	private List<User> users;
+	
+	public Customer() {}
+
+    public Customer(String customerName) {
+        this.name = customerName;
+    }
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}	
 }

1540c6f7341229293b4d5c03d843c7efac646830
Julia Takagi <takagi002@gannon.edu>
2022-06-06 20:31:13 +0200

    updated user ui

--- client/src/users/Users.jsx
+++ client/src/users/Users.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { withStyles } from '@material-ui/core/styles';
 import UserDetail from "./UserDetail";
 import {fetchCustomers, fetchUsers} from '../common/apiUtility';
+import { Button, Typography} from "@material-ui/core";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const styles = theme => ({
 		center: {
@@ -40,20 +43,28 @@ class Users extends React.Component {
 				this.state.customers.map((customer, index) => {
 					return (
 					<div class="customerGrid" key={index}>
-						<span>{customer.name}</span>
+						<Typography variant="body1" gutterBottom>
+						{customer.name}	
+     					</Typography>
 						<div>{
 							this.state.users.map((user, index) => {
 								if (user.customer)
 								{
 								return (
 								<div class="userGrid" key={index}>
-									<div>{user.firstname} {user.name}</div>
-									<div>{user.email}</div>
-									<button onClick={() => this.openEditor(user)}>Edit</button>
-									<button>Delete</button>
+									<div class='row-border'></div>
+									<Typography variant="body1" gutterBottom>
+									{user.firstname} {user.name}
+     					 			</Typography>
+									<Typography variant="body1" gutterBottom>
+									{user.email}
+     					 			</Typography>
+									<Button startIcon={<EditIcon />} onClick={() => this.openEditor(user)}>Edit</Button>
+									<Button startIcon={<DeleteIcon />} >Delete</Button>
 								</div>
 							)}})}
 						</div>
+						<div class='row-border'></div>
 					</div>
 				)})}
 				<UserDetail currentUser={this.state.selectedUser} isOpen={this.state.isEditing}></UserDetail>

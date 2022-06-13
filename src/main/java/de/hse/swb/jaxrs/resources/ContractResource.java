0a00a82fab7b7811b793616e7bc9e07f796d9d7e
Rudolf Baun <rubait01@hs-esslingen.de>
2022-06-13 14:46:13 +0200

    fixed contracts path parameters

--- src/main/java/de/hse/swb/jaxrs/resources/ContractResource.java
+++ src/main/java/de/hse/swb/jaxrs/resources/ContractResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -37,9 +38,9 @@ public class ContractResource {
     }
     
     @GET
-    @Path("id")
+    @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Contract getContract(Long id) {
+    public Contract getContract(@PathParam("id") long id) {
         return contractDao.getContract(id);
     }
 
@@ -69,9 +70,9 @@ public class ContractResource {
     
     
     @DELETE
-    @Path("id")
+    @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void deleteContract(Long id) {
+    public void deleteContract(@PathParam("id") long id) {
     	Contract contract = contractDao.getContract(id);
     	contractDao.removeContract(contract);
     }
